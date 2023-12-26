2059ee597a5c962693e8f2524b4daa69c5bd0d1b
omri <omrijabar28@gmail.com>
2023-12-26 04:36:03 +0200

    Update Dockerfile and docker-compose.yml

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ c = a + b
 print(f"{a} + {b} is {c}")
 
 print("program exited.")
+
+import mysql.connector
+import time
+cnx = mysql.connector.connect(user='root', password='El3nksamkfshab3!!!', host='localhost', database='masterdb')
+print(cnx)
+time.sleep(120)
